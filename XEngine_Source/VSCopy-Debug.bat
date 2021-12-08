2e4086da7efcc16fc856e58bdabf266b197e8569
qyt <486179@qq.com>
2021-12-08 16:36:26 +0800

    fixed:linux build modofy:vscopy file

---
+++ XEngine_Source/VSCopy-Debug.bat
@@ -0,0 +1,14 @@
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_BaseLib.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_Algorithm.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_Core.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_ManagePool.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_NetXApi.dll" "./"
+
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIHelp.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XClient_Socket.dll" "./"
+
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_XLog.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_ProxyProtocol.dll" "./"
+
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_ProcFile.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_SystemApi.dll" "./"
