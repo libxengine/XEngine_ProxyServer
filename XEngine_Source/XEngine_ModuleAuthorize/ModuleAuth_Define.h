b964258eef4ebb986ce4cdafe0571a980d61847a
qyt <486179@qq.com>
2021-12-29 13:40:14 +0800

    added:user authorize module

---
+++ XEngine_Source/XEngine_ModuleAuthorize/ModuleAuth_Define.h
@@ -0,0 +1,61 @@
+#pragma once
+/********************************************************************
+//    Created:     2021/12/29  10:30:04
+//    File Name:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleAuthorize\ModuleAuth_Define.h
+//    File Path:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleAuthorize
+//    File Base:   ModuleAuth_Define
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     导出定义
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                        导出函数
+//////////////////////////////////////////////////////////////////////////
+extern "C" DWORD ModuleAuthorize_GetLastError(int* pInt_SysError = NULL);
+/************************************************************************/
+/*                        验证导出函数                                  */
+/************************************************************************/
+/********************************************************************
+函数名称：ModuleAuthorize_User_Init
+函数功能：初始化授权会话管理器
+ 参数.一：lpszAuthFile
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入授权文件地址
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+extern "C" BOOL ModuleAuthorize_User_Init(LPCTSTR lpszAuthFile);
+/************************************************************************
+函数名称：ModuleAuthorize_User_Destory
+函数功能：销毁授权管理器
+返回值
+  类型：逻辑型
+  意思：是否销毁成功
+备注：
+************************************************************************/
+extern "C" BOOL ModuleAuthorize_User_Destory();
+/********************************************************************
+函数名称：ModuleAuthorize_User_Exist
+函数功能：是否验证通过
+ 参数.一：lpszUser
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入用户名
+ 参数.二：lpszPass
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入密码
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+extern "C" BOOL ModuleAuthorize_User_Exist(LPCTSTR lpszUser, LPCTSTR lpszPass);
---
+++ XEngine_Source/XEngine_ModuleAuthorize/pch.h
@@ -0,0 +1,43 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+#ifdef _WINDOWS
+// 添加要在此处预编译的标头
+#include "framework.h"
+#include <tchar.h>
+#else
+
+#endif
+#endif //PCH_H
+#include <string>
+#include <unordered_map>
+#include <XEngine_Include/XEngine_CommHdr.h>
+#include <XEngine_Include/XEngine_Types.h>
+#include <XEngine_Include/XEngine_ProtocolHdr.h>
+#include "ModuleAuth_Define.h"
+#include "ModuleAuth_Error.h"
+using namespace std;
+#ifdef _UNICODE
+typedef std::wstring tstring;
+#else
+typedef std::string tstring;
+#endif
+/********************************************************************
+//    Created:     2021/12/29  10:27:12
+//    File Name:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleAuthorize\pch.h
+//    File Path:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleAuthorize
+//    File Base:   pch
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     公用头文件
+//    History:
+*********************************************************************/
+extern BOOL Authorize_IsErrorOccur;
+extern DWORD Authorize_dwErrorCode;
