d6d15b26b5e68eac93c08f79e6c53ae286a2ee5a
qyt <486179@qq.com>
2022-06-08 09:21:30 +0800

    fixed:the problem that the copy file is imcomplete

--- XEngine_Source/VSCopy-x86.bat
+++ XEngine_Source/VSCopy-x86.bat
@@ -9,6 +9,7 @@ copy /y "%XEngine_Lib32%\XEngine_NetHelp\NetHelp_APIHelp.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Client\XClient_Socket.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\HelpComponents_XLog.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_HelpComponents\HelpComponents_Packets.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_RfcComponents\RfcComponents_ProxyProtocol.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_SystemSdk\XEngine_ProcFile.dll" "./"
@@ -20,4 +21,5 @@ copy /y "%XEngine_Lib32%\XEngine_SystemSdk\XEngine_SystemApi.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_LibEx\libcrypto-3.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_LibEx\libssl-3.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_LibEx\libcurl.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_LibEx\nghttp2.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\zlib1.dll" "./"
