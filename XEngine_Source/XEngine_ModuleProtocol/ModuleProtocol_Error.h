93757d1e1943faf518fbe95e65392aa1a993e673
qyt <486179@qq.com>
2022-06-09 09:52:05 +0800

    added:protocol module

---
+++ XEngine_Source/XEngine_ModuleProtocol/ModuleProtocol_Error.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2022/06/08  11:21:20
+//    File Name:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleProtocol\ModuleProtocol_Error.h
+//    File Path:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleProtocol
+//    File Base:   ModuleProtocol_Error
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     导出错误
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                        导出的打包错误
+//////////////////////////////////////////////////////////////////////////
+#define ERROR_MODULE_PROTOCOL_PACKET_PARAMENT 0xD0001                 //参数错误
+//////////////////////////////////////////////////////////////////////////
+//                        导出的解析错误
+//////////////////////////////////////////////////////////////////////////
+#define ERROR_MODULE_PROTOCOL_PARSE_PARAMENT 0xD1001                  //参数错误
+#define ERROR_MODULE_PROTOCOL_PARSE_JSON 0xD1002                      //解析JSON错误
---
+++ XEngine_Source/XEngine_ModuleProtocol/ModuleProtocol_Parse/ModuleProtocol_Parse.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2022/06/08  13:56:05
+//    File Name:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleProtocol\ModuleProtocol_Parse\ModuleProtocol_Parse.h
+//    File Path:   D:\XEngine_ProxyServer\XEngine_Source\XEngine_ModuleProtocol\ModuleProtocol_Parse
+//    File Base:   ModuleProtocol_Parse
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     解析协议类
+//    History:
+*********************************************************************/
+
+class CModuleProtocol_Parse
+{
+public:
+	CModuleProtocol_Parse();
+	~CModuleProtocol_Parse();
+public:
+	BOOL ModuleProtocol_Parse_ForwardBind(LPCTSTR lpszMsgBuffer, int nMsgLen, TCHAR* ptszSrcAddr, TCHAR* ptszDstAddr);
+private:
+};
