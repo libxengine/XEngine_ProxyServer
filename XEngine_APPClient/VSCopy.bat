b6bd24b33c9a5bc37056f4cbd3d5d1a4fe732d8b
qyt <486179@qq.com>
2022-06-15 13:29:58 +0800

    added:example vscopy file

---
+++ XEngine_APPClient/VSCopy.bat
@@ -0,0 +1,4 @@
+copy /y "%XEngine_Lib32%\XEngine_BaseLib\XEngine_BaseLib.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_BaseLib\XEngine_Algorithm.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_Client\XClient_Socket.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_RfcComponents\RfcComponents_ProxyProtocol.dll" "./"
