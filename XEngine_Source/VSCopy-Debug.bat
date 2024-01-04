38238a278e04e6461c4426c18ce6a3f0777d1354
qyt <486179@qq.com>
2024-01-04 14:12:27 +0800

    fixed:copyfile and make error

--- XEngine_Source/VSCopy-Debug.bat
+++ XEngine_Source/VSCopy-Debug.bat
@@ -6,11 +6,11 @@ copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_ManagePool.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_NetXApi.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIHelp.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIClient.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XClient_Socket.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_XLog.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_Packets.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\RfcComponents_ProxyProtocol.dll" "./"
 
-copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_ProcFile.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_SystemApi.dll" "./"
--- XEngine_Source/VSCopy-x86.bat
+++ XEngine_Source/VSCopy-x86.bat
@@ -6,11 +6,11 @@ copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_ManagePool.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_NetXApi.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_NetHelp\NetHelp_APIHelp.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_NetHelp\NetHelp_APIClient.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Client\XClient_Socket.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\HelpComponents_XLog.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_HelpComponents\HelpComponents_Packets.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_RfcComponents\RfcComponents_ProxyProtocol.dll" "./"
 
-copy /y "%XEngine_Lib32%\XEngine_SystemSdk\XEngine_ProcFile.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_SystemSdk\XEngine_SystemApi.dll" "./"
