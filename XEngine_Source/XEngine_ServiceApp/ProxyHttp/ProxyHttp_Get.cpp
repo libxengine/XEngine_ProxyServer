6f5524c31fff66eba728ff94f7471b8ab6937e75
qyt <486179@qq.com>
2026-07-30 17:35:35 +0800

    added:list function support and list forward for http api

--- XEngine_Source/XEngine_ServiceApp/ProxyHttp/ProxyHttp_Get.cpp
+++ XEngine_Source/XEngine_ServiceApp/ProxyHttp/ProxyHttp_Get.cpp
@@ -3,12 +3,41 @@
 bool XEngine_ProxyHTTP_Get(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR lpszAPIType, LPCXSTR lpszMsgBuffer, int nMsgLen)
 {
 	int nSDLen = 0;
-	LPCXSTR lpszAPIReload = _X("reload");
-	LPCXSTR lpszAPIProxyRule = _X("proxyrule");
 
+	LPCXSTR lpszAPIList = _X("list");
+	LPCXSTR lpszAPIReload = _X("reload");
 	XCHAR tszSDBuffer[XPATH_8MAX] = {};
 
-	if (0 == _tcsxncmp(lpszAPIReload, lpszAPIName, _tcsxlen(lpszAPIReload)))
+	if (0 == _tcsxncmp(lpszAPIList, lpszAPIName, _tcsxlen(lpszAPIList)))
+	{
+		LPCXSTR lpszAPIProxyRule = _X("proxyrule");
+		LPCXSTR lpszAPIForward = _X("forward");
+
+		if (0 == _tcsxncmp(lpszAPIProxyRule, lpszAPIName, _tcsxlen(lpszAPIProxyRule)))
+		{
+			//http://127.0.0.1:5400/api?function=proxyrule&type=0
+			int nListCount = 0;
+			SESSION_IPCONUT** ppSt_IPCountList;
+			ModuleSession_ProxyRule_GetList(&ppSt_IPCountList, &nListCount);
+			ModuleProtocol_Packet_GetProxyRuleList(tszSDBuffer, &nSDLen, &ppSt_IPCountList, nListCount);
+			XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
+			BaseLib_Memory_Free((XPPPMEM)&ppSt_IPCountList, nListCount);
+			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,发送获取转发规则列表请求处理成功"), lpszClientAddr);
+		}
+		else if (0 == _tcsxncmp(lpszAPIName, lpszAPIForward, _tcsxlen(lpszAPIForward)))
+		{
+			//http://127.0.0.1:5400/api?function=list&value=forward
+			int nListCount = 0;
+			SESSION_FORWARD** ppSt_ListUser;
+			ModuleSession_Forward_List(&ppSt_ListUser, &nListCount);
+
+			ModuleProtocol_Packet_ForwardList(tszSDBuffer, &nSDLen, &ppSt_ListUser, nListCount);
+			XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
+			BaseLib_Memory_Free((XPPPMEM)&ppSt_ListUser, nListCount);
+			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,转发列表请求成功"), lpszClientAddr);
+		}
+	}
+	else if (0 == _tcsxncmp(lpszAPIReload, lpszAPIName, _tcsxlen(lpszAPIReload)))
 	{
 		delete st_ProxyConfig.pStl_ListDestAddr;
 		delete st_ProxyConfig.pStl_ListRuleAddr;
@@ -18,17 +47,6 @@ bool XEngine_ProxyHTTP_Get(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR 
 		ModuleProtocol_Packet_Comm(tszSDBuffer, &nSDLen);
 		XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
 		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_WARN, _X("HTTP客户端:%s,发送重载配置请求成功"), lpszClientAddr);
-	}
-	else if (0 == _tcsxncmp(lpszAPIProxyRule, lpszAPIName, _tcsxlen(lpszAPIProxyRule)))
-	{
-		//http://127.0.0.1:5400/api?function=proxyrule
-		int nListCount = 0;
-		SESSION_IPCONUT** ppSt_IPCountList;
-		ModuleSession_ProxyRule_GetList(&ppSt_IPCountList, &nListCount);
-		ModuleProtocol_Packet_GetProxyRuleList(tszSDBuffer, &nSDLen, &ppSt_IPCountList, nListCount);
-		XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
-		BaseLib_Memory_Free((XPPPMEM)&ppSt_IPCountList, nListCount);
-		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,发送获取转发规则列表请求处理成功"), lpszClientAddr);
 	}
 	else
 	{
--- XEngine_Source/XEngine_ServiceApp/ProxyHttp/ProxyHttp_Post.cpp
+++ XEngine_Source/XEngine_ServiceApp/ProxyHttp/ProxyHttp_Post.cpp
@@ -4,21 +4,11 @@ bool XEngine_ProxyHTTP_Post(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR
 {
 	int nSDLen = 0;
 	LPCXSTR lpszAPIList = _X("list");
-	LPCXSTR lpszAPIForward = _X("forward");
 
 	XCHAR tszSDBuffer[XPATH_8MAX] = {};
 
-	if (0 == _tcsxncmp(lpszAPIName, lpszAPIForward, _tcsxlen(lpszAPIForward)))
+	if (0 == _tcsxncmp(lpszAPIName, lpszAPIList, _tcsxlen(lpszAPIList)))
 	{
-		//http://127.0.0.1:5400/api?function=list&value=forward
-		int nListCount = 0;
-		SESSION_FORWARD** ppSt_ListUser;
-		ModuleSession_Forward_List(&ppSt_ListUser, &nListCount);
-
-		ModuleProtocol_Packet_ForwardList(tszSDBuffer, &nSDLen, &ppSt_ListUser, nListCount);
-		XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
-		BaseLib_Memory_Free((XPPPMEM)&ppSt_ListUser, nListCount);
-		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,转发列表请求成功"), lpszClientAddr);
 	}
 	else
 	{
