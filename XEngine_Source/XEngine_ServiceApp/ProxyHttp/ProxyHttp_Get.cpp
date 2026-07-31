117d3181436d84b503662b7bf66156e2dea4b63e
qyt <486179@qq.com>
2026-07-31 14:10:04 +0800

    fixed:api name process failure

--- XEngine_Source/XEngine_ServiceApp/ProxyHttp/ProxyHttp_Get.cpp
+++ XEngine_Source/XEngine_ServiceApp/ProxyHttp/ProxyHttp_Get.cpp
@@ -14,9 +14,9 @@ bool XEngine_ProxyHTTP_Get(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR 
 		LPCXSTR lpszAPIProxy = _X("proxy");
 		LPCXSTR lpszAPIForward = _X("forward");
 
-		if (0 == _tcsxncmp(lpszAPIProxyRule, lpszAPIName, _tcsxlen(lpszAPIProxyRule)))
+		if (0 == _tcsxncmp(lpszAPIProxyRule, lpszAPIType, _tcsxlen(lpszAPIProxyRule)))
 		{
-			//http://127.0.0.1:5400/api?function=proxyrule&type=0
+			//http://127.0.0.1:5400/api?function=list&type=rule
 			int nListCount = 0;
 			SESSION_IPCONUT** ppSt_IPCountList;
 			ModuleSession_ProxyRule_GetList(&ppSt_IPCountList, &nListCount);
@@ -25,7 +25,7 @@ bool XEngine_ProxyHTTP_Get(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR 
 			BaseLib_Memory_Free((XPPPMEM)&ppSt_IPCountList, nListCount);
 			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,发送获取转发规则列表请求处理成功"), lpszClientAddr);
 		}
-		else if (0 == _tcsxncmp(lpszAPIName, lpszAPIForward, _tcsxlen(lpszAPIForward)))
+		else if (0 == _tcsxncmp(lpszAPIForward, lpszAPIType, _tcsxlen(lpszAPIForward)))
 		{
 			//http://127.0.0.1:5400/api?function=list&value=forward
 			int nListCount = 0;
@@ -37,7 +37,7 @@ bool XEngine_ProxyHTTP_Get(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR 
 			BaseLib_Memory_Free((XPPPMEM)&ppSt_ListUser, nListCount);
 			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,转发forward 列表请求成功"), lpszClientAddr);
 		}
-		else if (0 == _tcsxncmp(lpszAPIProxy, lpszAPIName, _tcsxlen(lpszAPIProxy)))
+		else if (0 == _tcsxncmp(lpszAPIProxy, lpszAPIType, _tcsxlen(lpszAPIProxy)))
 		{
 			//http://127.0.0.1:5400/api?function=list&value=proxy
 			int nListCount = 0;
@@ -47,6 +47,12 @@ bool XEngine_ProxyHTTP_Get(LPCXSTR lpszClientAddr, LPCXSTR lpszAPIName, LPCXSTR 
 			XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
 			BaseLib_Memory_Free((XPPPMEM)&ppSt_ListUser, nListCount);
 			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _X("HTTP客户端:%s,转发proxy 列表请求成功"), lpszClientAddr);
+		}
+		else
+		{
+			ModuleProtocol_Packet_Comm(tszSDBuffer, &nSDLen, ERROR_XENGINE_PROXY_PROTOCOL_NOTSUPPORT, _X("unknow protocol"));
+			XEngine_Network_Send(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_CLIENT_NETTYPE_HTTP);
+			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_WARN, _X("HTTP客户端:%s,发送GET请求名称:%s,类型:%s,处理失败,不支持"), lpszClientAddr, lpszAPIName, lpszAPIType);
 		}
 	}
 	else if (0 == _tcsxncmp(lpszAPIReload, lpszAPIName, _tcsxlen(lpszAPIReload)))
